07d5bdef62e33fdd69382c7d405c835af4230b25
Vadim Shushlyakov <shushlyakov@gmail.com>
2019-04-06 11:36:10 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('static'));
+app.use((req, res, next) => {
+  const auth = {login: 'dev', password: 'def'};
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+
+  if (!login || !password || login !== auth.login || password !== auth.password) {
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+    return
+  }
+  next()
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/static/_index.html');
+});
+
+app.get('/api', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
