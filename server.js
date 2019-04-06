29470f8de5e724bd670b78872059c36872575a3c
Vadim Shushlyakov <shushlyakov@gmail.com>
2019-04-06 21:16:48 +0300

    dev

--- server.js
+++ server.js
@@ -5,21 +5,40 @@ const multer = require('multer');
 const upload = multer({ dest: 'temp/uploads/' });
 
 const express = require('express');
-const { parseFileAsync } = require('./lib/dicom-parser');
-
+// const { parseFileAsync } = require('./server/dicom-parser');
+let nextId = 0;
 const app = express();
+const database = {
+  studies: [],
+};
 
 app.use(express.static('static'));
 app.use('/node_modules', express.static('node_modules'));
 
 app.post('/upload', upload.array('files'), (req, res) => {
-  console.log(req.files[0]);
   res.send(JSON.stringify(req.files[0]));
 });
 
-app.get('/api/parse', (req, res) => {
-  parseFileAsync(path.join(__dirname, 'data/01_1/IM000000'));
-  res.send('Hello World!');
+app.use(express.json());
+
+app.get('/study/list', (req, res) => {
+  res.send(JSON.stringify(database.studies));
+});
+
+app.get('/study/get', (req, res) => {
+  res.send(JSON.stringify(database.studies.find(i => i.id === +req.query.id)));
+});
+
+
+app.get('/study/getFile', (req, res) => {
+  res.sendFile(path.join(__dirname, req.query.path));
+});
+
+
+app.post('/study/add', (req, res) => {
+  req.body.id = ++nextId;
+  database.studies.push(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(8082, () => {
